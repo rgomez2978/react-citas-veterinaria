9f4c5bb4ca6c7f29a5b8f06403909e7716447c21
Ronald Gomez <rgomez2978@gmail.com>
2022-06-10 12:40:33 -0400

    Updates

---
+++ precache-manifest.c84475c6a21c6cc86112e96c62368ea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9517869f946bb8a798d5edd6b70dca",
+    "url": "/react-citas-veterinaria/index.html"
+  },
+  {
+    "revision": "7a84c46046f2d958b274",
+    "url": "/react-citas-veterinaria/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "e7a447256629a8379229",
+    "url": "/react-citas-veterinaria/static/js/2.b18ce891.chunk.js"
+  },
+  {
+    "revision": "7a84c46046f2d958b274",
+    "url": "/react-citas-veterinaria/static/js/main.077178aa.chunk.js"
+  },
+  {
+    "revision": "b4c1ac9b33ba1e083b60",
+    "url": "/react-citas-veterinaria/static/js/runtime-main.727959b7.js"
+  }
+]);
